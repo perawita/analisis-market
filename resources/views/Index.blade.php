b7f1b4b6f6288172a6513168472a90a17962dbb8
perawitayasa <perawitayasa@gmail.com>
2024-06-27 18:57:27 +0000

    h

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -17,11 +17,10 @@
 
     <div class="card">
         <div class="card-header">
-            <h2 class="pb-2 border-bottom">Layanan Kami</h2>
+           Layanan Kami
         </div>
         <div class="card-body">
             <div class="container px-4 py-5" id="hanging-icons">
-                <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
                     <div class="col d-flex align-items-start">
                         <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
                             <svg class="bi" width="1em" height="1em">
@@ -37,7 +36,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
     </div>
 

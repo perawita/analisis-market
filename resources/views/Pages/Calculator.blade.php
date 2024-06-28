e0d3ffc7edfb7337ce9aad5746e2d61f0ab11234
perawitayasa <perawitayasa@gmail.com>
2024-06-28 16:49:43 +0000

    fixed ui

--- resources/views/Pages/Calculator.blade.php
+++ resources/views/Pages/Calculator.blade.php
@@ -13,12 +13,9 @@
 
         <br>
 
-        <div class="card-header">
-            Calculator Symbols
-        </div>
+        @if($results)
         <div class="card-body">
             <div class="row g-5">
-                @if($results)
                 <div class="col-md-5 col-lg-4 order-md-last">
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
                         <span class="text-primary">Results</span>
@@ -47,39 +44,32 @@
                         </li>
                     </ul>
                 </div>
-                @endif
 
 
 
                 <div class="col-md-7 col-lg-8">
 
-                    <h4 class="mb-3">Margin of Safety (MoS)</h4>
-                    <form class="needs-validation" action="{{route('Calculator-result')}}" method="POST">
-                        @csrf
-                        <div class="row gy-3">
-                            <div class="col-md-6">
-                                <label for="eps" class="form-label">EPS</label>
-                                <input type="text" class="form-control" id="eps" name="eps" required="true">
-                            </div>
-
-                            <div class="col-md-3">
-                                <label for="growth_rate" class="form-label">Growth rate prjection</label>
-                                <input type="text" class="form-control" id="growth_rate" name="growth_rate" required="true">
-                            </div>
-
-                            <div class="col-md-6">
-                                <label for="current_stock_price" class="form-label">PE of company w/ no growth</label>
-                                <input type="text" class="form-control" id="current_stock_price" name="current_stock_price" required="true">
-                            </div>
+                    <h4 class="mb-3">Value Margin of Safety (MoS)</h4>
+                    <div class="row gy-3">
+                        <div class="col-md-6">
+                            <label for="eps" class="form-label">EPS</label>
+                            <input type="text" class="form-control" id="eps">
                         </div>
 
-                        <hr class="my-4">
+                        <div class="col-md-3">
+                            <label for="growth_rate" class="form-label">Growth rate prjection</label>
+                            <input type="text" class="form-control" id="growth_rate">
+                        </div>
 
-                        <button class="w-100 btn btn-primary btn-lg" type="submit">Get results</button>
-                    </form>
+                        <div class="col-md-6">
+                            <label for="current_stock_price" class="form-label">PE of company w/ no growth</label>
+                            <input type="text" class="form-control" id="current_stock_price">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        @endif
 
     </div>
 
